1dad3ca83181c7847afd3d6ea868e8d8ec00c163
Ryan Huard <rhuard13@gmail.com>
2016-07-21 14:19:05 -0700

    Update SuseLinux.sh

--- SuseLinux.sh
+++ SuseLinux.sh
@@ -1,1 +1,44 @@
+function make_dir(){
+    if [ ! -d $1 ]; then
+        mkdir $1
+    fi
+}
 
+#TODO: test this function
+function install(){
+    $INSTALL $1 -y
+}
+
+$INSTALL="sudo Zypper install"
+
+#Install software
+$INSTALL"-t pattern devel_basis"
+$INSTALL"python"
+$INSTALL"python3"
+$INSTALL"llvm-clang"
+$INSTALL"gcc"
+$INSTALL"dev86" #bcc
+$INSTALL"cmake"
+$INSTALL"vim"
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+$INSTALL""
+
+v
+$INSTALL""
