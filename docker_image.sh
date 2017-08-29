932b7da08676866a2645a86ee9cf41882bf9cd13
rhuard <ryan.huard@isilon.com>
2017-08-29 14:47:26 -0700

    add docker_image startup

---
+++ docker_image.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Requires git to run, not installed because if you have this then I assume you have git
+
+#TODO: figure out how to do switches better so can have more options
+sudo apt update
+
+INSTALL="sudo apt-get install "
+
+#INSTALL PROGRAMS AND TOOLS
+$INSTALL"build-essential" -y
+$INSTALL"clang" -y
+$INSTALL"python2.7" -y
+$INSTALL"python3" -y
+$INSTALL"python-pip" -y
+$INSTALL"gcc" -y
+$INSTALL"bcc" -y
+$INSTALL"cmake" -y
+$INSTALL"vim" -y
+$INSTALL"tmux" -y
+$INSTALL"zsh" -y
+$INSTALL"ack-grep" -y
+#set ack-grep to use the name ack
+sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
+$INSTALL"curl" -y
+$INSTALL"ranger" -y
+$INSTALL"pylint" -y
+$INSTALL"cowsay" -y
+$INSTALL"figlet" -y
+$INSTALL"exuberant-ctags" -y
+$INSTALL"colordiff" -y
+$INSTALL"diffuse" -y
+$INSTALL"source-highlight" -y
+$INSTALL"dict dictd dict-gcide" -y
+$INSTALL"dict-moby-thesaurus" -y
+$INSTALL"aspell" -y
+$INSTALL"feh" -y
+
+$HOME/NewStartup/Common.sh
