8bc8788bb4d2de808d06455fb0b4e32c961b3ebb
Ryan Huard <rhuard13@gmail.com>
2020-09-11 20:03:26 -0700

    Update and rename Arch.sh to ArchStart.sh

---
+++ ArchStart.sh
@@ -0,0 +1,24 @@
+INSTALL="sudo pacman -S "
+
+# right now all my laptops have built in intel chips, need to expand/add options for different platforms
+$INSTALL base-devel \
+mesa \
+lib32-mesa \
+vulkan-intel \
+xorg-server \
+lightdm \
+plasma-desktop \
+i3 \
+vivaldi-stable \
+firefox \
+terminator \
+tmux \
+ack \
+ranger \
+vim \
+ctags \
+aspell-en \
+dictd \
+dict-moby-thesaurus \
+
+$HOME/NewStartup/Common.sh
